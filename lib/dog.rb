6969f52faab67e88c99222c50190938303870371
Jc Dior <jessicaclairdior@gmail.com>
2020-09-15 21:47:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,19 @@
 class Dog
+
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
+    @dogs_name = dogs_name
   end
 
   def name
-    @this_dogs_name
+    @dogs_name
   end
 
   def breed=(breed_name)
-    @this_dogs_breed = breed_name
+    @dogs_breed = breed_name
   end
 
   def breed
-    @this_dogs_breed
+    @dogs_breed
   end
+
 end
